d23d3c2056360e9088b7ecebe149efcc7ef7204a
lonewolf <nikhilkalige@gmail.com>
2017-09-04 22:06:03 -0700

    Add spi tx

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! Prints "Hello, World" in the OpenOCD console
 //#![deny(unsafe_code)]
-#![deny(warnings)]
+//#![deny(warnings)]
+#![feature(const_fn)]
 #![feature(proc_macro)]
 #![no_std]
 
@@ -13,36 +14,82 @@ use core::fmt::Write;
 use rtfm::{app, Threshold};
 use semihosting::hio;
 use bsp::spi2;
+use bsp::dma2 as dma;
 use bsp::prelude::*;
 
 pub mod setup;
 
+
 app! {
     device: bsp::stm32f411,
+
+    resources: {
+        static BUFFER: dma::Buffer<[u8; 14], dma::DMA1Stream0> = dma::Buffer::new([0; 14]);
+    },
+
+    tasks: {
+        DMA2_STREAM2: {
+            path: transfer_done,
+            resources: [
+                BUFFER, 
+                DMA2],
+        },
+    },
+
     idle: {
         resources: [ITM, SPI1]
     }
 }
 
-fn init(p: init::Peripherals) {
-    setup::spi_setup(p);
-}
+// app! {
+//     device: bsp::stm32f411,
 
-fn idle(_t: &mut Threshold, r: idle::Resources) -> ! {
-    writeln!(hio::hstdout().unwrap(), "Hello, world!").unwrap();
-    let spi = spi2::Spi::<_, bsp::stm32f411::DMA1>::new(&*r.SPI1, spi2::Role::MASTER, None, None);
+//     tasks: {
+//         DMA1_STREAM3: {
+//             path: transfer_done,
+//             resources: [DMA1],
+//         },
+//     },
+
+//     idle: {
+//         resources: [ITM, SPI1]
+//     }
+// }
+
+fn init(p: init::Peripherals, r: init::Resources) {
+    writeln!(hio::hstdout().unwrap(), "Setting up peripherals").unwrap();
+    setup::spi_setup(&p);
+    setup::dma_setup(&p);
+    
+    let dma_tx = dma::Dma::new(p.DMA2, dma::DMAStream::Stream2);
+    
+    let spi = spi2::Spi::new(p.SPI1, spi2::Role::MASTER, None, Some(&dma_tx));
     spi.enable();
 
-    loop {
-        for i in 0..255 {
-            while spi.send(i).is_err() {}
-            while spi.read().is_err() {}
-            // writeln!(hio::hstdout().unwrap(), "Finished sending {}", i).unwrap();
-        }
-    }
+    r.BUFFER.borrow_mut().clone_from_slice(b"Hello, world!\n");
+    spi.send_dma(r.BUFFER).unwrap();
+}
 
-//    writeln!(hio::hstdout().unwrap(), "Finished sending").unwrap();
+fn idle(_t: &mut Threshold, _r: idle::Resources) -> ! {
+    // let spi = spi2::Spi::<_, bsp::stm32f411::DMA1>::new(&*r.SPI1, spi2::Role::MASTER, None, None);
+    // spi.enable();
+
     // loop {
-    //     rtfm::wfi();
+        // for i in 0..255 {
+            // while spi.send(i).is_err() {}
+            // while spi.read().is_err() {}
+            // writeln!(hio::hstdout().unwrap(), "Finished sending {}", i).unwrap();
+        // }
     // }
+
+   writeln!(hio::hstdout().unwrap(), "Finished sending").unwrap();
+    loop {
+        rtfm::wfi();
+    }
+}
+
+fn transfer_done(_t: &mut Threshold, r: DMA2_STREAM2::Resources) {
+    r.BUFFER.release(r.DMA2, dma::DMAStream::Stream2).unwrap();
+
+    // rtfm::bkpt();
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,10 +1,35 @@
 use bsp::stm32f411;
-extern crate bsp;
-
 use bsp::spi2;
-// use bsp::dma2;
+use bsp::dma2;
 
-pub fn spi_setup(p: stm32f411::Peripherals) {
+pub fn dma_setup(p: &stm32f411::Peripherals) {
+    p.RCC.ahb1enr.modify(|_, w| w.dma2en().set_bit());
+
+    let dma_rx = dma2::Dma::new(p.DMA2, dma2::DMAStream::Stream0);
+    let dma_tx = dma2::Dma::new(p.DMA2, dma2::DMAStream::Stream2);
+
+    dma_rx.channel(dma2::Channel::CHANNEL_3);
+    dma_rx.priority(dma2::Priority::MEDIUM);
+    dma_rx.memdata_alignment(dma2::DataSize::BYTE);
+    dma_rx.periphdata_alignment(dma2::DataSize::BYTE);
+    dma_rx.memory_increment(true);
+    dma_rx.direction(dma2::Direction::PERIPH_TO_MEMORY);
+    dma_rx.reg.s0cr.modify(|_, w| w.tcie().set_bit());
+    
+    dma_tx.channel(dma2::Channel::CHANNEL_2);
+    dma_tx.priority(dma2::Priority::MEDIUM);
+    dma_tx.memdata_alignment(dma2::DataSize::BYTE);
+    dma_tx.periphdata_alignment(dma2::DataSize::BYTE);
+    dma_tx.memory_increment(true);
+    dma_tx.direction(dma2::Direction::MEMORY_TO_PERIPH);
+    dma_tx.reg.s2cr.modify(|_, w| w.tcie().set_bit());
+
+    dma_rx.disable();
+    dma_tx.disable();
+
+}
+
+pub fn spi_setup(p: &stm32f411::Peripherals) {
     p.RCC.ahb1enr.modify(|_, w| w.gpioaen().set_bit());
     p.RCC.apb2enr.modify(|_, w| w.spi1en().set_bit());
 
@@ -35,5 +60,7 @@ pub fn spi_setup(p: stm32f411::Peripherals) {
     spi.data_size(spi2::DataSize::_8BIT);
     spi.msb_first(true);
     spi.crc_calculation(false);
-    spi.nss(spi2::NSS::HARD_OUTPUT);
+    spi.nss(spi2::NSS::HardOutput);
+
+    spi.reg.cr2.modify(|_, w| w.txdmaen().set_bit());
 }
